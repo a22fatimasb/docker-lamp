d0334821f71deddb132f50bffd711fcab6c6f709
Fátima Sánchez BLanco <a22fatimasb@iessanclemente.net>
2023-10-06 23:00:08 +0200

    actualización exercicios

--- www/ud02/01introduccion/tarea2.php
+++ www/ud02/01introduccion/tarea2.php
@@ -2,26 +2,87 @@
 
 /**
  * Indica cuál de los siguientes son nombres de variables válidas e inválidos, indica por qué (en comentarios) y corrige los fallos:
-- valor 
-- $_N
-- $valor_actual 
-- $n
-- $#datos 
-- $valorInicial0
-- $proba,valor 
-- $2saldo
-- $n
-- $meuProblema
-- $meu Problema
-- $echo
-- $m&m
-- $registro
-- $ABC
-- $85 Nome
-- $AAAAAAAAA
-- $nome_apelidos
-- $saldoActual
-- $92
-- $*143idade
+ valor = non é válido dado que tería que ter o símbolo dollar diante
+ $_N = é correcto.
+ $valor_actual  = é correcto
+ $n = é correcto
+ $#datos = non é válido, ten que empezar por unha letra ou unha barra baixa
+ $valorInicial0 = correcto
+ $proba,valor  = incorrecto non pode levar unha coma
+ $2saldo = incorrecto, non pode empezar por un número
+ $n = correcto
+ $meuProblema = correcto
+ $meu Problema = incorrecto, non pode ter un espacio
+ $echo = correcto
+ $m&m = incorrecto, ten un símbolo non admitido
+ $registro = correcto
+ $ABC = correcto
+ $85 Nome = incorrecto, dado que empeza por un número e ten un espazo no medio
+ $AAAAAAAAA = correcto
+ $nome_apelidos = correcto
+ $saldoActual = correcto
+ $92 = incorrecto, non pode empezar por un número
+ $*143idade = non válido, empeza por un símbolo non admitido.
  */
+
+ // Os nomes de valores válidos ou correxidos:
+
+ $valor = 0;
+ echo $valor;
+
+ $_N = 0;
+ echo $_N;
+
+ $valor_actual = 0;
+ echo $valor_actual;
+
+ $n = 0;
+ echo $n;
+
+ $datos = 0; 
+ echo $datos;
+
+ $valorInicial0 = 0;
+ echo $valorInicial0;
+
+ $proba_valor = 0;
+ echo $proba_valor;
+
+ $saldo = 0;
+ echo $saldo;
+ 
+ $meuProblema = 0;
+ echo $meuProblema;
+
+ $echo = 0;
+ echo $echo;
+
+ $mandm = 0;
+ echo $mandm;
+
+ $registro = 0;
+ echo $registro;
+ 
+ $ABC = 0;
+ echo $ABC;
+
+ $Nome85 = 0;
+ echo $Nome85;
+
+ $AAAAAAAAA = 0;
+ echo $AAAAAAAAA;
+
+ $nome_apelidos = 0;
+ echo $nome_apelidos;
+
+ $saldoActual = 0;
+ echo $saldoActual;
+
+ $noventaedeus = 92;
+ echo $noventaedeus;
+ 
+ $_143idade = 0;
+ echo $_143idade;
+
+
 ?>
--- www/ud02/01introduccion/tarea3.php
+++ www/ud02/01introduccion/tarea3.php
@@ -1,23 +1,43 @@
 <?php
-
 /**Busca en la documentación de PHP las funciones de [manejo de variables](http://www.php.net/manual/es/funcref.php)
 
 Comprueba el resultado devuelto por los siguientes fragmentos de código y analiza el resultado:
-```php
-- $a = “true”; // imprime el valor devuelto por is_bool($a)...
-- $c = “false”; // imprime el valor devuelto por gettype($c);
-- $d = “”; // el valor devuelto por empty($d);
-- $e = 0.0; // el valor devuelto por empty($e);
-- $f = 0; // el valor devuelto por empty($f);
-- $g = false; // el valor devuelto por empty($g);
-- $h; // el valor devuelto por empty($h);
-- $i = “0”; // el valor devuelto por empty($i);
-- $j = “0.0”; // el valor devuelto por empty($j);
-- $k = true; // el valor devuelto por isset($k);
-- $l = false; // el valor devuelto por isset($l);
-- $m = true; // el valor devuelto por is_numeric($m);
-- $n = “”; // el valor devuelto por is_numeric($n);
-```
- */
+*/
+ $a = "true"; // Comproba se unha variable é de tipo booleano. Se é verdadeiro devolve un 1 se é falso un 0 (o cal non se amosa por pantalla);
+ echo is_bool($a);
+ $c = "false"; // Devolve o tipo da variable, neste caso un String.
+ echo gettype($c);
+ 
+ $d = ""; // devolve un 1 polo tanto está a decirnos que está vacío.
+ echo empty($d);
+ 
+ $e = 0.0; // considerao vacío así que devolve 1
+ echo empty($e);
+ 
+ $f = 0; // considerao vacío así que devolve 1
+ echo empty($f);
+ $g = false; // considerao vacío así que devolve 1
+ echo empty($g);
+ 
+ $h; // devolve 1 dado que non o considera vacío
+ echo empty($h);
+
+ $i = 0; // considerao vacio así que devolve 1
+ echo empty($i);
+  
+ $j = "0.0"; // devolve 0 dado que non o considera vacío 
+ echo empty($j);
+
+ $k = true; //  determina se a variable está definida e non é null. Imprime por pantalla 1 dado que é verdadeiro.
+ echo isset($k);
+
+ $l = false; // Imprime por pantalla 1 dado que é verdadeiro.
+ echo isset($l);
+ 
+ $m = true; //  determina se é un número ou un string numérico. Neste caso devolve 0 porque non o é, non se imprime por pantalla.
+ echo is_numeric($m);
+
+ $n = ""; //  Neste caso devolve 0 porque non o é, non se imprime por pantalla.
+ echo is_numeric($n);
 
 ?>
