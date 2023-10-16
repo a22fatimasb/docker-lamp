ac8176b576de18f228e488be2462903b27091319
Fátima Sánchez BLanco <a22fatimasb@iessanclemente.net>
2023-10-16 16:06:27 +0200

    actualización exercicios

--- www/ud02/03funciones/tarea2.php
+++ www/ud02/03funciones/tarea2.php
@@ -9,4 +9,35 @@
  * Co resto da división anterior, obtemos a letra corresponde na seguinte táboa: 
  */
 
+ $letras = array('T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z','S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E');
+ 
+ function comprobar_nif($nif){
+    global $letras;
+
+    if(empty($nif) || is_null($nif) || !preg_match('/^\d{8}[A-Za-z]$/', $nif)){
+        return "<br> Error con el NIF proporcionado";
+    }
+    
+    $resto_nif = substr($nif,0, 8);
+    $letra_nif= strtoupper(substr($nif, -1));
+    $resultadoModulo = $resto_nif % 23;
+
+    foreach($letras as $posicion => $letra){
+        if($posicion === $resultadoModulo){
+          if($letra === $letra_nif){
+            return true;
+          }
+        } 
+    }
+    return false;
+ }
+ $dni = "44824980G";
+ $resultado = comprobar_nif($dni);
+ if($resultado === "<br> Error con el NIF proporcionado"){
+    echo comprobar_nif($dni);
+ } elseif($resultado === true){
+    echo "El NIF es correcto";
+ } elseif($resultado === false){
+    echo "El NIF es incorrecto";
+ }
 ?>
