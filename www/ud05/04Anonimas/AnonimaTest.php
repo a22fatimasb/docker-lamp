e8048e9051f474352caf19c850892a4a1cfe4e70
Fátima Sánchez BLanco <a22fatimasb@iessanclemente.net>
2024-02-05 11:17:33 +0100

    Engadido o método "perimetro"

--- www/ud05/04Anonimas/AnonimaTest.php
+++ www/ud05/04Anonimas/AnonimaTest.php
@@ -3,7 +3,9 @@
 $base
 $altura
 La clase tiene dos métodos:
-area(): devolve la (base * altura) / 2 .*/
+area(): devolve la (base * altura) / 2 .
+perimetro(): devolve la 2 * base + 2 * altura
+*/
 
 $obj = new class($base = 0, $altura = 0)
 {
@@ -15,17 +17,24 @@ $obj = new class($base = 0, $altura = 0)
         $this->base = $base;
         $this->altura = $altura;
     }
+
     public function area()
     {
         return $this->base * $this->altura / 2;
     }
-};
 
+    public function perimetro()
+    {
+        return ($this->base * 2) + ($this->altura * 2);
+    }
+};
 
 $triangulo1 = clone $obj;
 $triangulo1->__construct(3, 4);
-echo "</br>Área de triangulo 1: " . $triangulo1->area();
+echo "</br>Área de triángulo 1: " . $triangulo1->area();
+echo "</br>Perímetro de triángulo 1: " . $triangulo1->perimetro();
 
 $triangulo2 = clone $obj;
 $triangulo2->__construct(8, 10);
-echo "</br>Área de triangulo 2: " . $triangulo2->area();
+echo "</br>Área de triángulo 2: " . $triangulo2->area();
+echo "</br>Perímetro de triángulo 2: " . $triangulo2->perimetro();
