f2b90492b5d9aa09ced48dced510c50a780a5163
Fátima Sánchez BLanco <a22fatimasb@iessanclemente.net>
2024-03-26 17:10:35 +0100

    Ejercicio HOteles listo

---
+++ www/ud06/funcionesBDHoteles.php
@@ -0,0 +1,63 @@
+<?php
+
+function get_all_hotel() {
+    $setencia = Flight::db()->prepare("SELECT * FROM hoteles");
+    $setencia->execute();
+    $datos = $setencia->fetchAll();
+    Flight::json($datos);
+}
+
+function get_hotel_by_id($id) {
+    $setencia = Flight::db()->prepare("SELECT * FROM hoteles WHERE id = ?");
+    $setencia->bindParam(1, $id);
+    $setencia->execute();
+    $hotel = $setencia->fetch(PDO::FETCH_ASSOC);
+    Flight::json($hotel);
+}
+
+function add_hotel() {
+    $id = Flight::request()->data->id;
+    $hotel = Flight::request()->data->hotel;
+    $direccion = Flight::request()->data->direccion;
+    $email = Flight::request()->data->email;
+    $telefono = Flight::request()->data->telefono;
+    
+    $sql = "INSERT INTO hoteles(id, hotel, direccion, email, telefono) VALUES (?, ?, ?, ?, ?)";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $id);
+    $sentencia->bindParam(2, $hotel);
+    $sentencia->bindParam(3, $direccion);   
+    $sentencia->bindParam(4, $email);
+    $sentencia->bindParam(5, $telefono);
+    $sentencia->execute();
+   
+    Flight::jsonp(["Hotel agregado correctamente."]);
+}
+function delete_hotel() {
+    $id = Flight::request()->data->id;
+   
+    $sql = "DELETE FROM hoteles WHERE id=?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $id);
+    $sentencia->execute();
+   
+    Flight::jsonp(["Hotel eliminado con id: $id"]);
+}
+
+function update_hotel() {
+    $id = Flight::request()->data->id;
+    $direccion = Flight::request()->data->direccion;
+    $email = Flight::request()->data->email;
+    $telefono = Flight::request()->data->telefono;
+   
+    $sql = "UPDATE hoteles SET direccion=?, email=?, telefono=? WHERE id=?";
+    $sentencia = Flight::db()->prepare($sql);
+    $sentencia->bindParam(1, $direccion);
+    $sentencia->bindParam(2, $email);
+    $sentencia->bindParam(3, $telefono);
+    $sentencia->bindParam(4, $id);
+    $sentencia->execute();
+   
+    Flight::jsonp(["Cliente actualizado correctamente."]);
+}
+?>
--- www/ud06/index.php
+++ www/ud06/index.php
@@ -2,7 +2,8 @@
 
 
 require 'flight/autoload.php';
-require 'utilidadesBD.php'; 
+require 'funcionesBDClientes.php';
+require 'funcionesBDHoteles.php'; 
 
 Flight::register('db', 'PDO', array('mysql:host=db;dbname=pruebas','root','test'));
 
@@ -10,11 +11,25 @@ Flight::route('/', function () {
     echo 'Ejercicios tema 6';
 });
 
+// Clientes
 Flight::route('GET /clientes', 'get_all_clients');
 Flight::route('GET /clientes/@id', 'get_client_by_id');
 Flight::route('POST /clientes', 'add_client');
 Flight::route('DELETE /clientes', 'delete_client');
 Flight::route('PUT /clientes', 'update_client');
+
+// Hoteles
+/*
+
+POST: Se debe poder insertar un hotel en la base de datos.
+DELETE: Dado un id se debe poder eliminar un hotel.
+PUT: Se podrá modificar de un hotel sus direccion, email y teléfono.
+*/
+Flight::route('GET /hoteles', 'get_all_hotel');
+Flight::route('GET /hoteles/@id', 'get_hotel_by_id');
+Flight::route('POST /hoteles', 'add_hotel');
+Flight::route('DELETE /hoteles', 'delete_hotel');
+Flight::route('PUT /hoteles', 'update_hotel');
 
 Flight::start();
 ?>
