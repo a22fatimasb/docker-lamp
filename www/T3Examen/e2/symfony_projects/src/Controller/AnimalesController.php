79c30c28b4c989fb7c583100d3ddece7d3542db0
a22fatimasb <a22fatimasb@iessanclemente.net>
2024-05-27 21:17:03 +0200

    Ejercicio 2 parte pets listo

--- www/T3Examen/e2/symfony_projects/src/Controller/AnimalesController.php
+++ www/T3Examen/e2/symfony_projects/src/Controller/AnimalesController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class AnimalesController extends AbstractController
 {
@@ -13,22 +15,22 @@ class AnimalesController extends AbstractController
     public function __construct()
     {
         // Array de las películas 
-        $this->animales = [
+        $this->pets = [
             [
-                "name" =>  "Purrsloud",
+            "name" =>  "Purrsloud",
             "species" =>  "Cat",
             "favFoods" =>  ["wet food", "dry food", "<strong>any</strong> food"],
             "birthYear" =>  2016,
             "photo" =>  "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
             ],
             [
-                "name" =>  "Barksalot",
+            "name" =>  "Barksalot",
             "species" =>  "Dog",
             "birthYear" =>  2008,
             "photo" =>  "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
             ],
             [
-                "name" =>  "Meowsalot",
+            "name" =>  "Meowsalot",
             "species" =>  "Cat",
             "favFoods" =>  ["tuna", "catnip", "celery"],
             "birthYear" =>  2012,
@@ -41,20 +43,28 @@ class AnimalesController extends AbstractController
     #[Route('/pets')]
     public function pets()
     {
+        
 
         return $this->render('animales/pets.html.twig', [
-            'animales' => $this->animales
+            'pets' => $this->pets
         ]);
     }
 
     #[Route('/petscache')]
     
-    public function petscache(){
-
-
+    public function petscache(HttpClientInterface $httpClient, CacheInterface $cache): Response{
+        $pets = $cache->get('animales_data', function ($cacheItem) use ($httpClient) {
+            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/a22fatimasb/docker-lamp/main/www/T3Examen/pets.json');
+            $data = $response->toArray();
+            $cacheItem->expiresAfter(2);
+            return $data;
+        });
+       
         return $this->render('animales/petscache.html.twig', [
+            'pets'=> $pets
         ]);
     }
 }
 
+    
     
